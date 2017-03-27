44bfd850ca5ce5c5c5dda8ef6f50daa0859df399
chenjsh36 <chenjsh36@outlook.com>
2017-03-27 23:28:27 +0800

    德州扑克

--- thpoker/index.js
+++ thpoker/index.js
@@ -21,8 +21,31 @@ app.use(function *(next){
 
 // response
 
-app.use(function *(){
-  this.body = 'Hello World';
-});
+// app.use(function *(){
+//   this.body = 'Hello World';
+// });
+
+app.use(function *pageNotFound(next) {
+	yield next;
+
+	if (404 !== this.status) return;
+
+	this.status = 404;
+
+	switch(this.accepts('html', 'json')) {
+		case 'html':
+			this.type = 'html';
+			this.body = '<p>page not found</p>';
+			break;
+		case 'json': 
+			this.body = {
+				message: 'Page not found'
+			};
+			break;
+		default: 
+			this.type = 'text';
+			this.body = 'Page Not Found';
+	}
+})
 
 app.listen(3000);
